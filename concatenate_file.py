586bc7a2c5a1617ef9aec4e68f311ea00455f04c
randerson112358 <randerson112358@users.noreply.github.com>
2018-09-03 00:20:33 -0400

    Update concatenate_file.py

--- concatenate_file.py
+++ concatenate_file.py
@@ -5,8 +5,7 @@ for line in open("<full_path>/fit1.csv"):
 # now the rest:     
 for num in range(2,25):     
 	f = open("<full_path>/fit"+str(num)+".csv")     
-	next(f) # skip the header 
-        next(f) # skip the header    
+	next(f) # skip the header    
 	for line in f:          
 		fout.write(line)     
 	f.close() # not really needed 
