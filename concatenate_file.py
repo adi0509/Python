e7aa1b19af60bc1089e4f68a2aad9fa624b56f57
randerson112358 <randerson112358@users.noreply.github.com>
2018-09-03 00:26:26 -0400

    Update concatenate_file.py

--- concatenate_file.py
+++ concatenate_file.py
@@ -1,9 +1,12 @@
+# This program concatenates multiple files in the form fit1, fit2, fit3, ... fit24
+# into one file called out.csv
+
 fout=open("out.csv","a")
 # first file: 
 for line in open("<full_path>/fit1.csv"):     
 	fout.write(line) 
 # now the rest:     
-for num in range(2,25):     
+for num in range(2,25):    #You can change the number of files to concatenate by changing 25 to a different number Note: range(2,25) is exclusive [2,25)
 	f = open("<full_path>/fit"+str(num)+".csv")     
 	next(f) # skip the header    
 	for line in f:          
