9c38bc249580c90d38f784268f22daea2198f03f
randerson112358 <randerson112358@users.noreply.github.com>
2019-06-06 17:55:14 -0400

    Update decisionTree.py

--- DecisionTree/decisionTree.py
+++ DecisionTree/decisionTree.py
@@ -4,9 +4,6 @@
 #'buying price', 'maintenance price', 'number of doors', 'number of passengers that fit in the car', 'size of luggage capacity', and 'safety'
 
 
-# For mathematical calculation
-import numpy as np
-
 # The original dataset is the car evaluation dataset from http://archive.ics.uci.edu/ml/datasets/Car+Evaluation,
 # more specifically it is a .data file originally and you can download it from http://archive.ics.uci.edu/ml/machine-learning-databases/car/car.data
 # We will classify the quality or values column of the car, after switching all of the values from the original dataset to integers
@@ -25,8 +22,9 @@ import numpy as np
 
 #read more: https://medium.com/machine-learning-guy/using-decision-tree-method-for-car-selection-problem-5272675451f9
 
+# Import the dependencies / libraries
+import numpy as np
 import pandas as pd
-
 from sklearn.tree import DecisionTreeClassifier
 
 #Create a dataframe from the cars dataset / csv file
@@ -35,19 +33,22 @@ df = pd.read_csv('DataSets/Cars/car_integer_exceptY.csv')
 #print the first 5 rows of the data set
 print(df.head())
 
+# Split your data into the independent variable(s) and dependent variable
 X_train = df.loc[:,'buying':'safety'] #Gets all the rows in the dataset from column 'buying' to column 'safety'
 Y_train = df.loc[:,'values'] #Gets all of the rows in the dataset from column 'values'
 
 # The actual decision tree classifier
 tree = DecisionTreeClassifier(max_leaf_nodes=3, random_state=0)
 
-
+# Train the model
 tree.fit(X_train, Y_train)
 
 #data.buying[ data.buying == 'low'] = 1
 #data.buying[ data.buying == 'med'] = 2
 #data.buying[ data.buying == 'high'] = 3
 #data.buying[ data.buying == 'vhigh'] = 4
+
+# Make your prediction
 prediction = tree.predict([[4,3,2,1,2,3]])
 
 print('Printing the prediction: ')
